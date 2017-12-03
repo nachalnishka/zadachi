f9571678aab48c81c1f20bbbf03a5d7b133ec9ad
Eva Nachalnishka <nachalnishka@abv.bg>
2017-12-03 19:11:54 +0200

    zadacha4

---
+++ Program3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int f = 1;
+
+            for(int i=1;i<=n;i++)
+            {
+                f = f * i;
+            }
+            Console.WriteLine("n!={0}", f);
+            Console.ReadLine();
+        }
+    }
+}
