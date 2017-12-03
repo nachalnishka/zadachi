7bc55cf1f03666e0a70036f36b695d9d91a126f3
Eva Nachalnishka <nachalnishka@abv.bg>
2017-12-03 19:15:11 +0200

    zadacha5

---
+++ Program4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int x = 0;
+            int y = 0;
+            int z = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                x = y;
+                y = z;
+                z = x + y;
+            }
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
