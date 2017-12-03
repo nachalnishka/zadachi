2c9c7b68753a1972a5fc50d1deade6468d4fa0e2
Eva Nachalnishka <nachalnishka@abv.bg>
2017-12-03 18:56:56 +0200

    zadacha2

---
+++ Program1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("demand0=");
+            decimal Demand0 = decimal.Parse(Console.ReadLine());
+            Console.Write("demandunit=");
+            decimal Demandunit = decimal.Parse(Console.ReadLine());
+            Console.Write("costinitial=");
+            decimal Costinitial = decimal.Parse(Console.ReadLine());
+            Console.Write("costunit=");
+            decimal Costunit = decimal.Parse(Console.ReadLine());
+
+            decimal a = -Demandunit;
+            decimal b = Costunit * Demandunit + Demand0;
+            decimal c = -Costunit * Demand0 - Costinitial;
+
+            decimal x = -b / (2 * a);
+            decimal y = a * x * x + b * x + c;
+            Console.Write("x={0}", x);
+            Console.Write("y={0}", y);
+            Console.ReadLine();
+
+        }
+    }
+}
